dbec869746a765a5cb9855d285f2aacb3980c846
Caleb Wolthers <w0473807@nscc.ca>
2023-09-13 14:00:43 -0300

    added more code
    game that doesnt work

--- variables/Program.cs
+++ variables/Program.cs
@@ -10,11 +10,28 @@ namespace variables
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Console.ReadKey();
 
             int score;
 
+            score = 45;
+
+
+            if (score > 100)
+            {
+                Console.WriteLine("Score: " + score);
+                Console.WriteLine("Press any key to continue..");
+                Console.WriteLine("");
+                Console.WriteLine("Except the power button");
+                Console.ReadKey();
+            }
+            else {
+
+                Console.WriteLine("GAME OVER");
+                Console.WriteLine("");
+                Console.WriteLine("Get a job");
+                Console.ReadKey();
+
+            }
         }
     }
 }
